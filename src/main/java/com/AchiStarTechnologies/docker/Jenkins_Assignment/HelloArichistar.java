de9dd8107f5ee610f159f0f3059f53f8a2780411
First Last <shamsundarjoshi@ip-172-31-74-229.ec2.internal>
2021-09-02 16:16:29 +0000

    second change to check jenkins pipline

---
+++ src/main/java/com/AchiStarTechnologies/docker/Jenkins_Assignment/HelloArichistar.java
@@ -0,0 +1,13 @@
+package com.AchiStarTechnologies.docker.Jenkins_Assignment;
+
+public class HelloArichistar {
+	
+	
+	public static void main(String args[])
+	{
+	
+	System.out.println("Heloo Aristar");
+	}
+	
+
+}
